27e3ad00fe44d182b0f9124dc778c2124e85ff3c
Cassandra <cassandra.moore1516@gmail.com>
2025-10-11 13:20:55 -0500

    Refactor API to MVC structure: add controllers, routes, and update main server file. Remove Netlify/serverless references from README. Ready for environment variable entry and testing.

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import memeRoutes from "./routes/memeRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/memes", memeRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
