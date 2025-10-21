68a0e32504e32b64054bf42844b92af8c6e9b810
Cassandra <cassandra.moore1516@gmail.com>
2025-10-20 21:42:45 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import memeRoutes from "./src/routes/memeRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/memes", memeRoutes);
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
