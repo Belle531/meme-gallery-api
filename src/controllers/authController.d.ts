3924e792093de4170f0c5bab31dcfcb25ad87ad0
Cassandra <cassandra.moore1516@gmail.com>
2025-10-20 21:07:12 -0500

    remove dobule files from install

--- src/controllers/authController.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export declare function authenticateToken(req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined;
-export declare const register: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const login: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=authController.d.ts.map
